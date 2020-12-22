c43b971bd475a39b89bcfa35b55233d06ecc3373
Yair Eshel Cahansky <guruyaya@gmail.com>
2020-12-22 09:21:55 +0200

    Running web interface

--- run_web_interface.py
+++ run_web_interface.py
@@ -1,11 +1,13 @@
 from app import app
+import argparse
 
-parser = argparse.ArgumentParser()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
 
-parser.add_argument("-p", "--port", help="Set the port to run blockchain", default=8000, type=int)
-parser.add_argument("--host", help="Set the host to run blockchain", default="0.0.0.0")
-parser.add_argument("-d", help="Start with debug mode", action="store_true")
+    parser.add_argument("-p", "--port", help="Set the port to run blockchain", default=8000, type=int)
+    parser.add_argument("--host", help="Set the host to run blockchain", default="0.0.0.0")
+    parser.add_argument("-d", help="Start with debug mode", action="store_true")
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-app.run(debug=True, host=args.host, port=args.port)
+    app.run(debug=True, host=args.host, port=args.port)
